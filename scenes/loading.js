9cf1d7dcbe600f0f4a6657e1213e69d64fc7b466
Chris Fankhauser <seretogis@gmail.com>
2012-06-28 13:00:22 -0700

    initial commit

---
+++ scenes/loading.js
@@ -0,0 +1,53 @@
+Crafty.sprite(32,"images/dungeon.png", {
+    floor: [0,1],
+    wall1: [2,0],
+    stairs: [1,3]
+});
+
+Crafty.sprite(32,"images/characters.png", {
+    hero1: [5,3],
+    blob1: [4,7]
+});
+
+//the loading screen that will display while our assets load
+Crafty.scene("loading", function() {
+    Crafty.background("#FFF");
+    Crafty.load(["images/dungeon.png","images/characters.png"], function() {
+        Crafty.scene("main"); //when everything is loaded, run the main scene
+    });
+});
+
+function loadMap(file, callback) {
+    $.get(file,function(data) {
+        var level = [];
+        // Split out each row
+        $.each(data.split("\n"),function(y,row) {
+            var columns = row.split("\t");
+            level.push(columns);
+            // Then split out each column
+            $.each(columns,function(x,column) {
+                if(column) {
+                    Crafty.e("2D, Canvas, floor, Floor").attr({x:x * 32, y: y * 32});
+                } else {
+                    Crafty.e("2D, Canvas, wall1, Wall").attr({x:x * 32, y: y * 32});
+                }
+            });
+
+        });
+
+        // Phone back home when we're done
+        callback(level);
+    });
+}
+
+function populateLevel(chance,level) {
+    $.each(level,function(y,row) {
+        $.each(row,function(x,entry) {
+            if(entry != "") {
+                if((Math.random() < chance)) {
+                    Crafty.e("2D, Canvas, blob1, AI, Solid").attr({x:x*32, y:y*32, z:1}).moveChance(Math.random()*0.5);
+                }
+            }
+        });
+    });
+}
